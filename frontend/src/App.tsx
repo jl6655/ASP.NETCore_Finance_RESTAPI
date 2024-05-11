64de7875e3c1b29a34613519c703619957078a5f
Joseph Lin <149325305+jl6655@users.noreply.github.com>
2024-05-11 18:38:26 -0400

    Add search button and list of cards button which show companies basic financial infos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CardList from './Components/CardList/CardList';
+import Search from './Components/Search/Search';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Search />
+      <CardList />
     </div>
   );
 }
