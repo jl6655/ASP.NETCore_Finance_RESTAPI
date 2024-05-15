20c7dccbda6bbf329f24753de90f8f6cab1cd254
Joseph Lin <149325305+jl6655@users.noreply.github.com>
2024-05-15 18:19:32 -0400

    Created usable search form that receives stock ticker inputs through the 'enter' button

---
+++ frontend/src/Components/Search/Search.tsx
@@ -0,0 +1,19 @@
+import React, { ChangeEvent, useState, SyntheticEvent } from 'react'
+
+interface Props {
+  onSearchSubmit: (e: SyntheticEvent) => void;
+  search: string | undefined;
+  handleSearchChange: (e: ChangeEvent<HTMLInputElement>) => void;
+};
+
+const Search: React.FC<Props> = ({onSearchSubmit, search, handleSearchChange}: Props): JSX.Element => {
+  return (
+    <>
+      <form onSubmit={onSearchSubmit}>
+        <input value={search} onChange={handleSearchChange} />
+      </form>
+    </>
+  );
+}
+
+export default Search;
