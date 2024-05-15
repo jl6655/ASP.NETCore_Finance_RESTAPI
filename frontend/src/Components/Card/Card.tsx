0629e062cd6b5f337361e69084d93a4f65ade396
Joseph Lin <149325305+jl6655@users.noreply.github.com>
2024-05-15 18:27:41 -0400

    Make each card display basic stock info of each stock returned in api call

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { SyntheticEvent } from 'react';
 import "./Card.css";
+import { CompanySearch } from "../../company";
+import AddPortfolio from '../Portfolio/AddPortfolio/AddPortfolio';
 
 interface Props {
-    companyName: string;
-    ticker: string;
-    price: number;
+    id: string;
+    searchResult: CompanySearch;
+    onPortfolioCreate: (e: SyntheticEvent) => void;
 }
 
-const Card: React.FC<Props> = ({companyName, ticker, price}: Props): JSX.Element => {
-  return <div className="card">
+const Card: React.FC<Props> = ({id, searchResult, onPortfolioCreate}: Props): JSX.Element => {
+  return (
+  <div className="card">
+    <img alt="company logo" />
     <div className="details">
         <h2>
-            {companyName} ({ticker})
+            {searchResult.name} ({searchResult.symbol})
         </h2>
-        <p>${price}</p>
+        <p>{searchResult.currency}</p>
     </div>
     <p className="info">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, incidunt.
+        {searchResult.exchangeShortName} - {searchResult.stockExchange}
     </p>
+    <AddPortfolio onPortfolioCreate={onPortfolioCreate} symbol={searchResult.symbol}/>
     </div>
+  );
 }
 
 export default Card;
