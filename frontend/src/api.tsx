4497386f2d2566d1989116ddc169bf8828f4c65e
Joseph Lin <149325305+jl6655@users.noreply.github.com>
2024-05-22 22:03:38 -0400

    Make new api endpoint that retrieves data for individual company cashflow statement

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,9 +1,8 @@
 import axios from "axios"
-import { CompanyProfile, CompanySearch } from "./company";
+import { CompanyBalanceSheet, CompanyCashFlow, CompanyIncomeStatement, CompanyKeyMetrics, CompanyProfile, CompanySearch } from "./company";
 
 interface SearchResponse {
     data: CompanySearch[];
-
 }
 
 export const searchCompanies = async (query : string ) => {
@@ -27,6 +26,50 @@ export const getCompanyProfile = async(query: string) => {
     try {
         const data = await axios.get<CompanyProfile[]> (
             `https://financialmodelingprep.com/api/v3/profile/${query}?apikey=${process.env.REACT_APP_API_KEY}`
+        )
+        return data;
+    } catch (error: any) {
+        console.log("Error message from API: ", error.message);
+    }
+}
+
+export const getKeyMetrics = async(query: string) => {
+    try {
+        const data = await axios.get<CompanyKeyMetrics[]> (
+            `https://financialmodelingprep.com/api/v3/key-metrics-ttm/${query}?limit=40&apikey=${process.env.REACT_APP_API_KEY}`
+        )
+        return data;
+    } catch (error: any) {
+        console.log("Error message from API: ", error.message);
+    }
+}
+
+export const getIncomeStatement = async(query: string) => {
+    try {
+        const data = await axios.get<CompanyIncomeStatement[]> (
+            `https://financialmodelingprep.com/api/v3/income-statement/${query}?limit=50&apikey=${process.env.REACT_APP_API_KEY}`
+        )
+        return data;
+    } catch (error: any) {
+        console.log("Error message from API: ", error.message);
+    }
+}
+
+export const getBalanceSheet = async(query: string) => {
+    try {
+        const data = await axios.get<CompanyBalanceSheet[]> (
+            `https://financialmodelingprep.com/api/v3/balance-sheet-statement/${query}?limit=40&apikey=${process.env.REACT_APP_API_KEY}`
+        )
+        return data;
+    } catch (error: any) {
+        console.log("Error message from API: ", error.message);
+    }
+}
+
+export const getCashFlowStatement = async(query: string) => {
+    try {
+        const data = await axios.get<CompanyCashFlow[]> (
+            `https://financialmodelingprep.com/api/v3/cash-flow-statement/${query}?limit=40&apikey=${process.env.REACT_APP_API_KEY}`
         )
         return data;
     } catch (error: any) {
