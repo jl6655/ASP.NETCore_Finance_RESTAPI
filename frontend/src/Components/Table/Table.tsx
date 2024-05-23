c74d5456c8ef1eed6a6bb4abbf128dc6ed9ca184
Joseph Lin <149325305+jl6655@users.noreply.github.com>
2024-05-22 22:01:38 -0400

    Make table use actual data from api calls and remove test data

--- frontend/src/Components/Table/Table.tsx
+++ frontend/src/Components/Table/Table.tsx
@@ -1,39 +1,22 @@
 import React from 'react'
-import { testIncomeStatementData } from './testData'
 
-const data = testIncomeStatementData;
+interface Props {
+    config: any;
+    data: any;
+};
 
-type Props = {};
-
-type Company = (typeof data)[0];
-
-const configs = [
-    {
-        label: "Year",
-        render: (company: Company) => company.acceptedDate
-    },
-    {
-        label: "Cost of Revenue",
-        render: (company: Company) => company.costOfRevenue
-    },
-];
-
-const Table = (props: Props) => {
-    const renderedRows = data.map((company) => {
+const Table = ({ config, data }: Props) => {
+    const renderedRows = data.map((company: any) => {
         return (
             <tr key={company.cik}>
-                {configs.map((val : any) => {
-                    return (
-                        <td className="p-4 whitespace-nowrap text-sm font-normal text-gray-900">
-                            {val.render(company)}
-                        </td>
-                    );
+                {config.map((val: any) => {
+                    return <td className="p-3">{val.render(company)}</td>;
                 })}
             </tr>
         );
     });
 
-    const renderedHeaders = configs.map((config: any) => {
+    const renderedHeaders = config.map((config: any) => {
         return (
             <th 
             className="p-4 text-left text-xs font-medium text-gray-500 uppercase tracking-wider" 
@@ -43,6 +26,7 @@ const Table = (props: Props) => {
             </th>
         )
     });
+    
   return (
     <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8">
         <thead className="min-w-full divide-y divide=gray-200 m-5">
