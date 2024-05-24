620de78b6124b08da537096b9c71237f5df26c84
Joseph Lin <149325305+jl6655@users.noreply.github.com>
2024-05-24 12:16:25 -0400

    Add companyTenK to company page

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -6,6 +6,7 @@ import Sidebar from '../../Components/Sidebar/Sidebar';
 import CompanyDashboard from '../../Components/CompanyDashboard/CompanyDashboard';
 import Tile from '../../Components/Tile/Tile';
 import Spinner from '../../Components/Spinner/Spinner';
+import TenKFinder from '../../Components/TenKFinder/TenKFinder';
 
 interface Props {}
 
@@ -31,6 +32,8 @@ const CompanyPage = (props: Props) => {
           <Tile title="Price" subTitle={company.price.toString()}></Tile>
           <Tile title="Sector" subTitle={company.sector}></Tile>
           <Tile title="Discounted Cashflow" subTitle={company.dcf.toString()}></Tile>
+          
+          <TenKFinder ticker={company.symbol}/>
           <p className="bg-white shadow rounded text-medium text-gray-900 p-3 mt-1 m-4">
             {company.description}
           </p>
