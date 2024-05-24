d55d03bee422903c60154607305ae328ecd2c6a8
Joseph Lin <149325305+jl6655@users.noreply.github.com>
2024-05-24 12:14:09 -0400

    Add CompanyTenK data type

--- frontend/src/company.d.ts
+++ frontend/src/company.d.ts
@@ -308,3 +308,13 @@ export interface CompanySearch {
     dividendPerShareTTM: number;
     debtToMarketCapTTM: number;
   }
+
+  export interface CompanyTenK {
+    symbol: string;
+    fillingDate: string;
+    acceptedDate: string;
+    cik: string;
+    type: string;
+    link: string;
+    finalLink: string;
+  }
