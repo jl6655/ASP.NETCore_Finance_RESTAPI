08c80f0668215a54f5824616d556874762fd224a
Joseph Lin <149325305+jl6655@users.noreply.github.com>
2024-05-25 15:35:20 -0400

    Create Comment and Stock objects

---
+++ api/Models/Stock.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public string Symbol { get; set; } = string.Empty;
+        public string CompanyName { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(18,2)")] // Ensures the monetary amount returned is within 18 digits and 2 decimal places
+        public decimal Purchase { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal LastDiv { get; set; }
+        public string Industry { get; set; } = string.Empty;
+        public long MarketCap { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>
+    }
+}
