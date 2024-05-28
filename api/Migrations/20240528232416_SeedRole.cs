b39f34b403174b698ff824c7e38d06c8b056a0f1
Joseph Lin <149325305+jl6655@users.noreply.github.com>
2024-05-28 19:30:17 -0400

    Add migrations to database for user registers

---
+++ api/Migrations/20240528232416_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "8a8e0e9c-267b-4d4a-9d07-4599b4e4e920", null, "Admin", "ADMIN" },
+                    { "9968a5d2-4be1-4370-8c9d-5b16c71f70a6", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "8a8e0e9c-267b-4d4a-9d07-4599b4e4e920");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "9968a5d2-4be1-4370-8c9d-5b16c71f70a6");
+        }
+    }
+}
