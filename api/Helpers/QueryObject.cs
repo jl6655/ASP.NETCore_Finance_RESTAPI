a73560e46512176f4436ca78c74a4532428ed132
Joseph Lin <149325305+jl6655@users.noreply.github.com>
2024-05-28 15:56:40 -0400

    Add ability to sort stocks by symbol or company name when return list of stocks in the database

---
+++ api/Helpers/QueryObject.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Helpers
+{
+    public class QueryObject
+    {
+        public string? Symbol { get; set; } = null;
+        public string? CompanyName { get; set; } = null;
+        public string? SortBy { get; set; } = null;
+        public bool IsDescending { get; set; } = false;
+    }
+}
--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -54,6 +54,23 @@ namespace api.Repository
                 stocks = stocks.Where(s => s.Symbol.Contains(query.Symbol));
             }        
 
+            if(!string.IsNullOrWhiteSpace(query.SortBy))
+            {
+                if(query.SortBy.Equals("Symbol", StringComparison.OrdinalIgnoreCase))
+                {
+                    stocks = query.IsDescending ? 
+                        stocks.OrderByDescending(s => s.Symbol) : 
+                        stocks.OrderBy(s => s.Symbol);
+                }
+
+                if(query.SortBy.Equals("CompanyName", StringComparison.OrdinalIgnoreCase))
+                {
+                    stocks = query.IsDescending ? 
+                        stocks.OrderByDescending(s => s.CompanyName) : 
+                        stocks.OrderBy(s => s.CompanyName);
+                }
+            }
+
             return await stocks.ToListAsync();
         }
 
