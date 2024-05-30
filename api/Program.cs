67807bce5cacc2d03f35a934234123718e1694a1
Joseph Lin <149325305+jl6655@users.noreply.github.com>
2024-05-30 17:05:34 -0400

    Add dependency injection for portfolios

--- api/Program.cs
+++ api/Program.cs
@@ -90,6 +90,8 @@ builder.Services.AddAuthentication(options => {
 builder.Services.AddScoped<IStockRepository, StockRepository>();
 builder.Services.AddScoped<ICommentRepository, CommentRepository>();
 builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IPortfolioRepository, PortfolioRepository>();
+
 
 var app = builder.Build();
 
