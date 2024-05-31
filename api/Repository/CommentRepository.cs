02383687048745a9af2e53367f76dd4d92c1dd95
Joseph Lin <149325305+jl6655@users.noreply.github.com>
2024-05-31 17:13:07 -0400

    Revise each GetAll and GetById function to include the AppUser when retrieving from database

--- api/Repository/CommentRepository.cs
+++ api/Repository/CommentRepository.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
-using api.Dtos.Comment;
 using api.Interfaces;
 using api.Models;
 using Microsoft.EntityFrameworkCore;
@@ -43,13 +42,13 @@ namespace api.Repository
 
         public async Task<List<Comment>> GetAllAsync() 
         {
-            return await _context.Comments.ToListAsync();
+            return await _context.Comments.Include(a => a.AppUser).ToListAsync();
         }
 
 
         public async Task<Comment?> GetByIdAsync(int id)
         {
-            return await _context.Comments.FindAsync(id);
+            return await _context.Comments.Include(a => a.AppUser).FirstOrDefaultAsync(c => c.Id == id);
         }
 
         public async Task<Comment?> UpdateAsync(int id, Comment commentModel)
