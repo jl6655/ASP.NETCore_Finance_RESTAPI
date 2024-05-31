2228e2afc6479e883c21401ef56073fa118c8af7
Joseph Lin <149325305+jl6655@users.noreply.github.com>
2024-05-31 17:11:26 -0400

    Add marker that ties each comment to the user it was created by using a one-to-one relation

--- api/Dtos/Comment/CommentDto.cs
+++ api/Dtos/Comment/CommentDto.cs
@@ -11,6 +11,7 @@ namespace api.Dtos.Comment
         public string Title { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
         public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public string CreatedBy { get; set; } = string.Empty;
         public int? StockId { get; set; } 
         // Don't want the navigation property ('Stock? stock) because it would inject a whole object within
         // the commentDto (visual appeals)
