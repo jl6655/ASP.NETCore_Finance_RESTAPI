7d1f49955e6c33db53b43d1416b8d0dd2b6622cf
Joseph Lin <149325305+jl6655@users.noreply.github.com>
2024-05-31 22:09:47 -0400

    Revise Add function so that if the searched stock isn't within the stock database, it uses the Financial Modeling Prep API to validate the stock and if the stock exists, then it adds the stock within the stock database and add it to the user's portfolio. Otherwise, it returns an IActionResult that states the stock doesn't exist

--- api/Controllers/PortfolioController.cs
+++ api/Controllers/PortfolioController.cs
@@ -18,13 +18,17 @@ namespace api.Controllers
         private readonly UserManager<AppUser> _userManager;
         private readonly IStockRepository _stockRepo;
         private readonly IPortfolioRepository _portfolioRepo;
+        private readonly IFMPService _fmpService;
+
 
         public PortfolioController(UserManager<AppUser> userManager,
-        IStockRepository stockRepo, IPortfolioRepository portfolioRepo)
+        IStockRepository stockRepo, IPortfolioRepository portfolioRepo,
+        IFMPService fmpService)
         {
             _userManager = userManager;
             _stockRepo = stockRepo;
             _portfolioRepo = portfolioRepo;
+            _fmpService = fmpService;
         }
 
         [HttpGet]
@@ -44,6 +48,19 @@ namespace api.Controllers
             var username = User.GetUsername();
             var appUser = await _userManager.FindByNameAsync(username);
             var stock = await _stockRepo.GetBySymbolAsync(symbol);
+
+            if(stock == null)
+            {
+                stock = await _fmpService.FindStockBySymbolAsync(symbol);
+                if(stock == null) 
+                {
+                    return BadRequest("Stock does not exist");
+                }
+                else 
+                {
+                    await _stockRepo.CreateAsync(stock);
+                }
+            }
 
             if(stock == null)
             {
