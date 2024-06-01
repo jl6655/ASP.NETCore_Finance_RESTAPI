cc02665481908ed6e7132797af727dd2bbb20342
Joseph Lin <149325305+jl6655@users.noreply.github.com>
2024-06-01 17:35:13 -0400

    Add more understandable error handling messages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from 'react-router';
 import './App.css';
+import "react-toastify/dist/ReactToastify.css";
 import Navbar from './Components/Navbar/Navbar';
+import { ToastContainer } from 'react-toastify';
 
 
 function App() {
   return (
     <>
-    <Navbar /> 
-    <Outlet />
+      <Navbar /> 
+      <Outlet />
+      <ToastContainer />
     </>
   );
 }
