23f350a2666c06f1240797d48a9490613f237da7
Joseph Lin <149325305+jl6655@users.noreply.github.com>
2024-06-01 16:44:22 -0400

    Revise GetAll function of the COMMENT object to return query related comments and add ability to choose to display the comments in descending order

--- api/Repository/CommentRepository.cs
+++ api/Repository/CommentRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.Helpers;
 using api.Interfaces;
 using api.Models;
 using Microsoft.EntityFrameworkCore;
@@ -40,9 +41,21 @@ namespace api.Repository
             return commentModel;
         }
 
-        public async Task<List<Comment>> GetAllAsync() 
+        public async Task<List<Comment>> GetAllAsync(CommentQueryObject queryObject) 
         {
-            return await _context.Comments.Include(a => a.AppUser).ToListAsync();
+            var comments = _context.Comments.Include(a => a.AppUser).AsQueryable();
+            
+            if(!string.IsNullOrWhiteSpace(queryObject.Symbol))
+            {
+                comments = comments.Where(s => s.Stock.Symbol == queryObject.Symbol);
+            }
+
+            if(queryObject.IsDescending == true)
+            {
+                comments = comments.OrderByDescending(c => c.CreatedOn);
+            }
+
+            return await comments.ToListAsync();
         }
 
 
