3b9e1a13b35680ef7b69c9a234a328cffffd9a8d
Joseph Lin <149325305+jl6655@users.noreply.github.com>
2024-05-31 22:12:52 -0400

    Revise CREATE function so that if the searched stock isn't within the stock database, it uses the Financial Modeling Prep API to validate the stock and if the stock exists, then it adds the stock within the stock database and add the created comment to the comment database. Otherwise, it returns an IActionResult that states the stock doesn't exist

--- api/Controllers/CommentController.cs
+++ api/Controllers/CommentController.cs
@@ -19,13 +19,16 @@ namespace api.Controllers
         private readonly ICommentRepository _commentRepo;
         private readonly IStockRepository _stockRepo;
         private readonly UserManager<AppUser> _userManager;
+        private readonly IFMPService _fmpService;
 
         public CommentController(ICommentRepository commentRepo, 
-            IStockRepository stockRepo, UserManager<AppUser> userManager)
+            IStockRepository stockRepo, UserManager<AppUser> userManager,
+            IFMPService fmpService)
         {
             _commentRepo = commentRepo;
             _stockRepo = stockRepo;
             _userManager = userManager;
+            _fmpService = fmpService;
         }
 
         [HttpGet]
@@ -59,21 +62,31 @@ namespace api.Controllers
             return Ok(comment.ToCommentDto());
         }
 
-        [HttpPost("{stockId:int}")]
-        public async Task<IActionResult> Create([FromRoute] int stockId, CreateCommentDto commentDto) 
+        [HttpPost("{symbol:alpha}")]
+        public async Task<IActionResult> Create([FromRoute] string symbol, CreateCommentDto commentDto) 
         {
             if(!ModelState.IsValid) 
                 return BadRequest(ModelState); // Data Validation
 
-            if(!await _stockRepo.StockExists(stockId)) 
+            var stock = await _stockRepo.GetBySymbolAsync(symbol);
+
+            if(stock == null)
             {
-                return BadRequest("Stock does not exist");
+                stock = await _fmpService.FindStockBySymbolAsync(symbol);
+                if(stock == null) 
+                {
+                    return BadRequest("Stock does not exist");
+                }
+                else 
+                {
+                    await _stockRepo.CreateAsync(stock);
+                }
             }
 
             var username = User.GetUsername();
             var appUser = await _userManager.FindByNameAsync(username);
 
-            var commentModel = commentDto.ToCommentFromCreate(stockId); 
+            var commentModel = commentDto.ToCommentFromCreate(stock.Id); 
             commentModel.AppUserId = appUser.Id;
 
             await _commentRepo.CreateAsync(commentModel);
