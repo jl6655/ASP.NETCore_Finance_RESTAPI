e50b79e5748003b52970712fdd966e193b507290
Joseph Lin <149325305+jl6655@users.noreply.github.com>
2024-06-01 16:44:38 -0400

    Add finishing middleware touches

--- api/Program.cs
+++ api/Program.cs
@@ -94,7 +94,7 @@ builder.Services.AddScoped<IPortfolioRepository, PortfolioRepository>();
 builder.Services.AddScoped<IFMPService, FMPService>();
 builder.Services.AddHttpClient<IFMPService, FMPService>();
 
-var app = builder.Build();
+var app = builder.Build(); // Everything after 'builder.Build()' is considered middleware
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -104,6 +104,14 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .AllowCredentials()
+    //.WithOrigins("https://localhost:44351) <= What to use when deploying 
+    .SetIsOriginAllowed(origin => true)
+);
 
 app.UseAuthentication();
 app.UseAuthorization();
